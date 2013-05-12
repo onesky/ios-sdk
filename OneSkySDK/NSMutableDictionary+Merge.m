9c681e9d3408be083f4b1922d25fc65ee1de0840
Dev <git@dev>
2013-05-12 23:17:25 +0800

    Fix merge bug

--- OneSkySDK/NSMutableDictionary+Merge.m
+++ OneSkySDK/NSMutableDictionary+Merge.m
@@ -15,11 +15,8 @@
 - (void)mergingWithDictionary:(NSDictionary *)dict
 {
     for (id key in [dict allKeys]) {
-        id obj = [dict objectForKey:key];
-        id localObj = [self objectForKey:key];
-        // Ensure localObj is mutable dictionary for merging
-        localObj = [self mutableDictionaryCopyIfNeeded:localObj];
-        obj = [self mutableDictionaryCopyIfNeeded:localObj];
+        id obj = [self mutableDictionaryCopyIfNeeded:[dict objectForKey:key]];
+        id localObj = [self mutableDictionaryCopyIfNeeded:[self objectForKey:key]];
         if ([obj isKindOfClass:[NSDictionary class]] &&
             [localObj isKindOfClass:[NSMutableDictionary class]]) {
             // Recursive merge for NSDictionary
